6dd110b720afa18c7da8efdc427b27b9e512deb8
ashheartsyouu <38353327+ashheartsyouu@users.noreply.github.com>
2018-11-28 11:08:34 -0500

    Add files via upload

---
+++ controller/LocationServlet.java
@@ -0,0 +1,69 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dbHelpers.LocationQuery;
+import dbHelpers.ReadLocation;
+import model.Location;
+
+/**
+ * Servlet implementation class LocationServlet
+ */
+@WebServlet(description = "Takes information about zip code", urlPatterns = { "/LocationServlet" , "/search" })
+public class LocationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LocationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Location location = new Location();
+		
+		//get data
+		int zip = Integer.getInteger(request.getParameter("zip"));
+		
+		//readLocation
+		ReadLocation rl= new ReadLocation("name", "root", "password", "Zip");
+		
+		//get data
+		rl.doRead();
+		
+		Location results = rl.getLocation();
+		
+		//pass grocery and control to the updateform.jsp
+		request.setAttribute("location", results);
+		
+		String url = "/locationResult.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
+				
+
+		
+		
+	}
+
+}
