323ce04b18612336c7830c8ce068fb3947a2f08b
ashheartsyouu <38353327+ashheartsyouu@users.noreply.github.com>
2018-11-29 18:17:09 -0500

    Update ReadLocation.java

--- dbHelpers/ReadLocation.java
+++ dbHelpers/ReadLocation.java
@@ -13,7 +13,7 @@ public class ReadLocation {
 	private Location location = new Location();
 	private String state;
 		
-	public ReadLocation(String dbName, String username, String pwd, String SKU) {
+	public ReadLocation(String dbName, String username, String pwd, String state) {
 		String url = "jdbc:mysql://127.0.0.1:3306/" + dbName + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 		this.state= state;
 			
@@ -33,31 +33,49 @@ public class ReadLocation {
 				PreparedStatement ps = connection.prepareStatement(query);
 				ps.setString(1, this.state);
 				this.results = ps.executeQuery();
-				this.results.next();
-				location.setName(this.results.getString("Name"));
-				location.setAddress1(this.results.getString("Address 1"));
-				location.setAddress2(this.results.getString("Address 2"));
-				location.setCity(this.results.getString("City"));
-				location.setState(this.results.getString("State"));
-				location.setZip(this.results.getInt("Zip"));
+				if(this.results.next()) {
+				  location.setLocationID(this.results.getInt("locationID"));
+				location.setName(this.results.getString("locationName"));
+				location.setAddress1(this.results.getString("streetLine1"));
+				location.setAddress2(this.results.getString("streetLine2"));
+				location.setCity(this.results.getString("city"));
+				location.setState(this.results.getString("state"));
+				location.setZip(this.results.getInt("zip"));
+				} 
+				
 				
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}		
+		}
+		
+		public void doCheck() {
+			String query = "select * from Location";
+			
+			try {
+				PreparedStatement ps = connection.prepareStatement(query);
+				this.results = ps.executeQuery();
+				this.results.next();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 		public String getHTMLTable() {
 			String table = "";
 			table += "<table border = 1>";
 			
 			try {
+				results.beforeFirst();
 				while(this.results.next()) {
 					Location location = new Location();
 					location.setLocationID(this.results.getInt("locationID"));
-					location.setName(this.results.getString("name"));
-					location.setAddress1(this.results.getString("address1"));
-					location.setAddress2(this.results.getString("address2"));
+					location.setName(this.results.getString("locationName"));
+					location.setAddress1(this.results.getString("streetLine1"));
+					location.setAddress2(this.results.getString("streetLine2"));
 					location.setCity(this.results.getString("city"));
 					location.setState(this.results.getString("state"));
 					location.setZip(this.results.getInt("zip"));
