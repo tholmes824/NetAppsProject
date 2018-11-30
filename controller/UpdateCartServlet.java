80d6f043521bbb830ea1f28d179531cdce57fdf1
egc04856 <egc04856@uga.edu>
2018-11-30 14:30:47 -0500

    Update UpdateCartServlet.java

--- controller/UpdateCartServlet.java
+++ controller/UpdateCartServlet.java
@@ -8,15 +8,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import model.CartItems;
 import dbHelpers.ReadCart;
+import dbHelpers.UpdateCart;
 
 /**
  * Servlet implementation class UpdateFormServlet
  */
 @WebServlet(description = "This will get a product and use the data to fill in a table for updating the cart", urlPatterns = { "/updateCart" })
 public class UpdateCartServlet extends HttpServlet {
+	
+	private HttpSession session; 
+	
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -38,24 +43,23 @@ public class UpdateCartServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// get the custID
-		String custID = request.getParameter("custID");
+		int prodID = Integer.parseInt(request.getParameter("prodID"));
+		int quantity = Integer.parseInt(request.getParameter("quantity"));
 		
-		// create ReadRecord class
-		ReadCart rc = new ReadCart("d2decoyhhc", "root", "#1LOVEschool", custID);
+		CartItems ci = new CartItems();
+		ci.setQuantity(quantity);
+		ci.setProdID(prodID);
 		
-		// Use ReadRecord to get the product data
-		rc.doRead();
-		
-		CartItems cartItems = rc.getCartItems();
-		
-		// pass Product and control to the updateForm.jsp
-		request.setAttribute("cartItems", cartItems);
-		
-		String url = "/updateForm.jsp";
-		
+		// create an UpdateQuery object and use it to update the product
+		UpdateCart uc = new UpdateCart("d2decoy", "root", "#1LOVEschool");
+		uc.doUpdate(ci);
+
+		// pass control on to the ReadServlet
+		String url = "/read";
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
 		dispatcher.forward(request, response);
+		
 		
 	}
 
