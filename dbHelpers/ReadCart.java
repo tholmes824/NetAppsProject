c05970dbb388ab4183b83dbca8531a7c08d9d604
ashheartsyouu <38353327+ashheartsyouu@users.noreply.github.com>
2018-11-30 09:51:19 -0500

    Add files via upload

--- dbHelpers/ReadCart.java
+++ dbHelpers/ReadCart.java
@@ -5,16 +5,19 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import model.CartItems;
+import model.Customer;
+import model.Location;
 import model.Cart;
 
 public class ReadCart {
 	
 	private Connection connection;
 	private ResultSet results;
-	
 	private CartItems cartItems = new CartItems();
+	private Customer cust = new Customer();
 	private String cartID;
 	
 	
@@ -40,17 +43,13 @@ public class ReadCart {
 		
 		try {
 			PreparedStatement ps = connection.prepareStatement(query);
-			
 			ps.setString(1, this.cartID);
-			
 			this.results = ps.executeQuery();
-			
 			this.results.next();
 			
 			cartItems.setCartID(this.results.getInt("cartID"));
 			cartItems.setProdID(this.results.getInt("prodID"));
 			cartItems.setQuantity(this.results.getInt("quantity"));
-			
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -58,10 +57,45 @@ public class ReadCart {
 		}
 	}
 	
+	public String getHTMLTable() {
+		String table = "";
+		table += "<table border = 1>";
+		
+		try {
+			results.beforeFirst();
+			while(this.results.next()) {
+				CartItems cartItems = new CartItems();
+				cartItems.setCartID(this.results.getInt("cartID"));
+				cartItems.setProdID(this.results.getInt("prodID"));
+				cartItems.setQuantity(this.results.getInt("quantity"));
+
+				table += "<tr>";
+				table += "<td>";
+					table += cartItems.getCartID();
+				table += "</td>";
+				table += "<td>";
+					table += cartItems.getProdID();
+				table += "</td>";
+				table += "<td>";
+					table += cartItems.getQuantity();
+				table += "</td>";
+					table += "<a href=update?quantity=" + cartItems.getQuantity() + ">Update</a> <a href=delete?prodID=" + cartItems.getProdID() + ">Delete</a>";
+				table += "</tr>";
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		table += "</table>";
+		
+		return table;
+		}
+
+	
 	public CartItems getCartItems(){
 		return this.cartItems;
 	}
-	
 	
 
 }
